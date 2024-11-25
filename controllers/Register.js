284a67fa889734759bae711b6a1004e7e5fcad77
ghnimihassen <ghnimih@gmail.com>
2024-11-25 19:28:57 +0100

    first commit

--- controllers/Register.js
+++ controllers/Register.js
@@ -2,11 +2,12 @@ const User = require("../models/User");
 
 const register = async (req, res) => {
     const { email, password } = req.body;
-
+// Create a new user
     try {
         const user = new User({ email, password });
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
+        
     } catch (err) {
         res.status(500).json({ message: 'Error registering user', error: err.message });
     }
